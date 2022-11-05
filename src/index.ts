30c09eba6834541a19746e86682571cf5a5177a9
flaviafogaca <flaviamariafogaca@gmail.com>
2022-11-05 15:12:23 -0400

    modifica index

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,23 @@
 // Desaio Zenklub: Tweet Storm
 
 // Passo 1: Declarar o texto (tweet)
-const msg = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
+const text = 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.'
 
 // Passo 2: Criar função para criar array que separa a cada 140 caracteres sem cortar as palavras
-function msgTw(value: string): RegExpMatchArray | null {
+function textTweet(value: string): RegExpMatchArray | null {
   const finalValue = value.match(/.{1,130}(\s|$)/g)
   console.log(finalValue)
   return finalValue
 }
 
-msgTw(msg)
+textTweet(text)
 
 // Passo 3:Criar função para gerenciar paginação
-const msgPag = msgTw(msg)
-const msgPagAt = msgPag!.map(function (e, i) {
-  return (`${e}[${i + 1}/${msgPag!.length}]`)
+const textPagination = textTweet(text)
+const textFinal = textPagination!.map(function (e, i) {
+  return (`${e}[${i + 1}/${textPagination!.length}]`)
 })
 
-console.log(msgPagAt)
+console.log(textFinal)
 
 // Sugestão de melhoria - Passo 4: fazer o array respeitar 140 caracteres após paginação
